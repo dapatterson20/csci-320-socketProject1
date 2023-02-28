18ce0dc38a4835537d6c91bc4e26e74a8927dc6c
Derick Patterson <dapatterson20@students.desu.edu>
2023-02-28 11:57:51 -0500

    Hopefully this is right!

--- udp_server.py
+++ udp_server.py
@@ -15,15 +15,27 @@ def get_file_info(data: bytes) -> (str, int):
 def upload_file(server_socket: socket, file_name: str, file_size: int):
     # create a SHA256 object to verify file hash
     # TODO: section 1 step 5 in README.md file
-
+    hsh=hashlib.sha256(file_size)
     # create a new file to store the received data
     with open(file_name+'.temp', 'wb') as file:
         # TODO: section 1 step 7a - 7e in README.md file
-        pass  # replace this line with your code for section 1 step 7a - 7e
-
+        try:
+            while len(file.read(BUFFER_SIZE)) > 0:
+                chunk = file.read(BUFFER_SIZE)
+                file.write(chunk)
+                hsh.update(chunk)
+                server_socket.sendto(b'received', (IP, PORT))
+        except KeyboardInterrupt as ki:
+            print("Shutting down...")
     # get hash from client to verify
     # TODO: section 1 step 8 in README.md file
+    response2, client_address = server_socket.recvfrom(BUFFER_SIZE)
     # TODO: section 1 step 9 in README.md file
+    if response2==hsh:
+        server_socket.sendto(b'success', (IP, PORT))
+    else:
+        os.remove(file_name+'.temp')
+        server_socket.sendto(b'failed', (IP, PORT))
 
 
 def start_server():
@@ -36,8 +48,14 @@ def start_server():
         while True:
             # TODO: section 1 step 2 in README.md file
             # expecting an 8-byte byte string for file size followed by file name
+            name, client_address = server_socket.recvfrom(1024)
+
             # TODO: section 1 step 3 in README.md file
+            file_size = get_file_info(name[:8])
+            file_name = get_file_info(name[8:])
+            #file_size = int.from_bytes(file_size, byteorder='big')
             # TODO: section 1 step 4 in README.md file
+            server_socket.sendto(b'go ahead', client_address)
             upload_file(server_socket, file_name, file_size)
     except KeyboardInterrupt as ki:
         pass
